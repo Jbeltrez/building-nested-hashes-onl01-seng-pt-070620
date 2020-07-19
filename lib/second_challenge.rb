945be21182957353153e4ad21441c4a7b4fdbf28
john beltrez <beltrezjohn@gmail.com>
2020-07-19 04:30:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,30 @@
 def second_challenge
   epic_tragedy = {
-    
+    :montague => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :hero => {
+        #your key/value pairs here
+        },
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+   }
+  }
 
   
 end
